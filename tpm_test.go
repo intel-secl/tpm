93fc9f296567ceef6cd82d5ebb7aee7ac8b09bcc
David Zech <david.c.zech@intel.com>
2018-12-03 15:51:03 -0800

    Implement CI
    commit 839cf6a8bb4563ad87728939e58b790bccc60209
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 15:42:23 2018 -0800
    
        Increase sleep time to 3
    
    commit 06c35f84df353899626a76e9f3b6a49cf75911bb
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 15:35:08 2018 -0800
    
        Reorder stages
    
    commit fba9b113942806d770c7de5bf33e5f834ae19e6d
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 15:13:35 2018 -0800
    
        Edit job name
    
    commit 56d375afeea50c14e450c7210392c483a6aae99a
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 14:52:31 2018 -0800
    
        Add missing quote
    
    commit 1ec2215684cac1545cab41d7329a6b0660448351
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 14:36:26 2018 -0800
    
        Update test
    
    commit 2809bc50907ff7c869c83bd31d3dcc81716503d2
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 13:49:01 2018 -0800
    
        Rename ci file
    
    commit 5423b3a8ff13e647449aa81febbd80411c79c832
    Author: David Zech <david.c.zech@intel.com>
    Date:   Mon Dec 3 13:46:39 2018 -0800
    
        Add ci file

--- tpm_test.go
+++ tpm_test.go
@@ -3,16 +3,19 @@ package tpm
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func Test_Tpm12(t *testing.T) {
+func TestTpm12(t *testing.T) {
 	Config.UseSimulator = true
 	Config.SimulatorVersion = V12
 
 	tpm, err := Open()
-	assert.Nil(t, err)
-	if err != nil {
+	assert.NoError(t, err)
+	if err == nil {
 		defer tpm.Close()
 		bk, err := tpm.CreateCertifiedKey(Binding, []byte{'1', '2', '3', '4'}, []byte{'1', '2', '3', '4'})
-		assert.Nil(t, err)
+		assert.NoError(t, err)
+		if err != nil {
+			return
+		}
 		assert.NotEmpty(t, bk.PublicKey)
 		assert.NotEmpty(t, bk.PrivateKey)
 		assert.NotEmpty(t, bk.KeySignature)
@@ -20,9 +23,11 @@ func Test_Tpm12(t *testing.T) {
 		assert.Empty(t, bk.KeyName)
 		assert.Equal(t, bk.Usage, Binding)
 		assert.Equal(t, bk.Version, V12)
-
 		sk, err := tpm.CreateCertifiedKey(Signing, []byte{'1', '2', '3', '4'}, []byte{'1', '2', '3', '4'})
-		assert.Nil(t, err)
+		assert.NoError(t, err)
+		if err != nil {
+			return
+		}
 		assert.NotEmpty(t, sk.PublicKey)
 		assert.NotEmpty(t, sk.PrivateKey)
 		assert.NotEmpty(t, sk.KeySignature)
@@ -33,16 +38,19 @@ func Test_Tpm12(t *testing.T) {
 	}
 }
 
-func Test_Tpm20(t *testing.T) {
+func TestTpm20(t *testing.T) {
 	Config.UseSimulator = true
 	Config.SimulatorVersion = V20
 
 	tpm, err := Open()
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	if err != nil {
 		defer tpm.Close()
 		bk, err := tpm.CreateCertifiedKey(Binding, []byte{'1', '2', '3', '4'}, []byte{'1', '2', '3', '4'})
-		assert.Nil(t, err)
+		assert.NoError(t, err)
+		if err != nil {
+			return
+		}
 		assert.NotEmpty(t, bk.PublicKey)
 		assert.NotEmpty(t, bk.PrivateKey)
 		assert.NotEmpty(t, bk.KeySignature)
@@ -50,9 +58,11 @@ func Test_Tpm20(t *testing.T) {
 		assert.Empty(t, bk.KeyName)
 		assert.Equal(t, bk.Usage, Binding)
 		assert.Equal(t, bk.Version, V20)
-
 		sk, err := tpm.CreateCertifiedKey(Signing, []byte{'1', '2', '3', '4'}, []byte{'1', '2', '3', '4'})
-		assert.Nil(t, err)
+		assert.NoError(t, err)
+		if err != nil {
+			return
+		}
 		assert.NotEmpty(t, sk.PublicKey)
 		assert.NotEmpty(t, sk.PrivateKey)
 		assert.NotEmpty(t, sk.KeySignature)
